4a26cbc8b52ee6653c2fb16010cf1f15aef249ca
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2016-04-14 00:59:50 +0200

    no message

--- layout/header.php
+++ layout/header.php
@@ -1,23 +1,26 @@
-<div class="banner topFixed" id="banner_top">
-    <!--## Select country box ##-->
-    <div class="b-contry">
-        <div class="<?php echo strtolower($GLOBALS['country']) ?> flag"></div>
-
-        <select name="country_select" id="country_select" onchange="valueselect(this.value);"">
-        <?php foreach ($avail_CT as $key => $value) {
-            if ($key != "XX") { ?>
-                <option value="<?php echo $key ?>" <? if ($key == $GLOBALS['country']) {
-                    echo "selected=\"selected\"";
-                } ?>> <?php echo $value ?></option>
-            <?php }
-        } ?>
-        </select>
-    </div>
+<div id="banner_top">
+    <div id="left_top_div">&nbsp;</div>
     <!--#####################-->
     <div id="header_main_title">
         <h1 class="main_title">
             <? echo $MAIN_TITLE ?>
         </h1>
+    </div>
+    <!--## Select country box ##-->
+    <div id="country_box">
+        <div id="country_box_inline">
+            <div id="banner_flag" class="<?php echo strtolower($GLOBALS['country']) ?> flag"></div>
+
+            <select name="country_select" id="country_select" onchange="valueselect(this.value);"">
+            <?php foreach ($avail_CT as $key => $value) {
+                if ($key != "XX") { ?>
+                    <option value="<?php echo $key ?>" <? if ($key == $GLOBALS['country']) {
+                        echo "selected=\"selected\"";
+                    } ?>> <?php echo $value ?></option>
+                <?php }
+            } ?>
+            </select>
+        </div>
     </div>
 </div>
 <div class="description p3">
