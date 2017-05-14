239470f8ec8ccd6b9c5d7aecd6c8772997ef6bdf
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2017-05-15 00:26:14 +0200

    improvements on smooth transition between form parts

--- js/documentFunctions.js
+++ js/documentFunctions.js
@@ -9,15 +9,15 @@ function reload(onDocumentLoad) {
     //shows the form
     $("#input_div").show();
     //hides the results divs and correspondent class
-    $("#monthly_costs, #result_buttons_div, #pie_chart_div, #bar_chart_div").hide();
+    $("#monthly_costs, #result_buttons_div, #pie_chart_div, #bar_chart_div").hide("slow");
     $(".result_section").hide();
     
     ResultIsShowing=false;
         
     //reloaded to the original screen after the result tables were already shown
     if(!onDocumentLoad){
-        $("#div1").css("display", "block");
-        $("#div3").css("display", "block");
+        $("#div1").show();
+        $("#div3").show();
         $("#description").html(DescriptionHTML);
         resized();
         
@@ -123,48 +123,69 @@ var hasLoadedPart = [false, false, false, false]; //global array variable for fu
 var hasShownPart2 = false; var hasShownPart3 = false; //put to true when form part is FIRST shown
 function openForm_part(part_name, part_number_origin, part_number_destiny) {
 
-    //shows form part n and hides the other parts
-    function shows_part(n){
-        
-        if(n==1){
-            $('#div1_td, #div3_td').show();
-            $("#description").html(DescriptionHTML);
-        }
-        
-        if(n==2 || n==3){
-            $('#div1_td, #div3_td').hide();
-            $("#description").html("");            
-        }
+    //shows form part {d} coming from form part {o} hiding the remaining part
+    function shows_part(){
+        //origin and destiny form parts
+        var o=part_number_origin; 
+        var d=part_number_destiny; 
         
         //if not a test triggers event for Google Analytics accordingly
         if(!IsThisAtest() && IsGoogleAnalytics){
-            if(n==2 && !hasShownPart2){
+            if(d==2 && !hasShownPart2){
                 ga("send", "event", "form_part", "form_part_2");
                 hasShownPart2=true;
             }
-            if(n==3 && !hasShownPart3){
+            if(d==3 && !hasShownPart3){
                 ga("send", "event", "form_part", "form_part_3");
                 hasShownPart3=true;
             }
         }
         
-        //shows #form_part{n} and hides the other parts
-        var i = 1, p = $("#"+part_name+(i).toString());
-        while (p.length != 0){
-            
-            if (i == n){
-                p.show("slow");
-               
-            }
-            else{
-                p.hide("slow");        
-            }
-            i++;
-            p = $("#"+part_name+(i).toString());
+        //gets jQuery variable for each form part
+        var p1 = $("#"+part_name+"1");
+        var p2 = $("#"+part_name+"2");
+        var p3 = $("#"+part_name+"3");
+        
+        //makes smooth transitions between form parts
+        if (o==0 && d==1){ //when it loads
+            p1.slideDown("slow");
+            //in form part 1 shows the lateral information and the top description
+            $('#div1_td, #div3_td').show("slow");
+            $("#description").html(DescriptionHTML);
+            p2.hide();
+            p3.hide();
+        }
+        else if (o==1 && d==2){           
+            p1.slideUp("slow", function(){
+                $("#description").html("");           
+                $('#div1_td, #div3_td').hide("slow", function(){
+                    p2.slideDown("slow");
+                });                 
+            });
+        }
+        else if(o==2 && d==3){
+            p2.slideUp("slow", function(){
+                p3.slideDown("slow");
+            });           
+        }
+        else if(o==3 && d==2){
+            p3.slideUp("slow", function(){
+                p2.slideDown("slow");
+            });            
+        }
+        else if(o==2 && d==1){
+            p2.slideUp("slow", function(){
+                $("#description").hide().html(DescriptionHTML).slideDown("fast", function(){
+                                    p1.slideDown("slow", function(){
+                    $('#div1_td, #div3_td').show("slow");
+                });  
+                });                         
+            });            
         }
 
         scrollPage();
     }
+    
 
     //change from form part 1 to 2
     if (part_number_origin===1 && part_number_destiny===2){
@@ -182,13 +203,13 @@ function openForm_part(part_name, part_number_origin, part_number_destiny) {
                 hasLoadedPart[0] = true;
                 if (!is_userdata_formpart1_ok())
                     return;
-                shows_part(2);
+                shows_part();
             });                                             
         }
         else{
             if (!is_userdata_formpart1_ok())
                 return;
-            shows_part(2);
+            shows_part();
         }
         
         if (!hasLoadedPart[1]){
@@ -236,16 +257,16 @@ function openForm_part(part_name, part_number_origin, part_number_destiny) {
             if (IsGoogleCharts){
                 google.charts.load('current', {"packages": ["corechart"], "language": Language, "callback": function(){
                     hasLoadedPart[2]=true;
-                    shows_part(3);
+                    shows_part();
                 }});
             }
             else{
                 hasLoadedPart[2]=true;
-                shows_part(3);                
+                shows_part();                
             }
         }
         else{
-            shows_part(3);
+            shows_part();
         }
         
         if (!hasLoadedPart[3]){
@@ -282,17 +303,17 @@ function openForm_part(part_name, part_number_origin, part_number_destiny) {
     
     //change from form part 3 to 2
     if (part_number_origin==3 && part_number_destiny==2){
-        shows_part(2);
+        shows_part();
     }
     
     //change from form part 2 to 1
     if (part_number_origin==2 && part_number_destiny==1){
-        shows_part(1);
+        shows_part();
     }    
 
     //when it starts/loads the website
     if (part_number_origin===0 && part_number_destiny===1){
-        shows_part(1);
+        shows_part();
         scrollPage();
     }
     
@@ -317,13 +338,15 @@ function isVisible(html_ref) {
 //when number of inspections is zero in form part 1, hides field for cost of each inspection
 $("#numberInspections").focusout(function(){
     if( $(this).val() == 0){
-        $("#InspectionCost_tr").css("display", "none");
+        $("#InspectionCost_tr").hide("slow");
     }
     else{
         $("#InspectionCost_tr").css("display", "table-row");
     }
 });
 
+//function for the radio button with the question 
+//'Calculations based on:' in the Fuel section of Form part 2
 function fuelCalculationMethodChange(fuelCalculationMethod) {
     if (fuelCalculationMethod === "currency") {
         //selects actively radio button to which this function is associated
@@ -331,8 +354,8 @@ function fuelCalculationMethodChange(fuelCalculationMethod) {
         
         isDistanceSet = false;
         $("#distance_form3").each(function(){ $(this).show(); });
-        $("#currency_div_form2").css("display", "block");
-        $("#distance_div_form2").css("display", "none");
+        $("#currency_div_form2").slideDown("slow");
+        $("#distance_div_form2").slideUp("slow");
         $(".time_spent_part1_form3").each(function(){ $(this).hide(); });
         $(".time_spent_part2_form3").show();
         $("#drive_to_work_no_form3").prop("checked", true);
@@ -344,8 +367,8 @@ function fuelCalculationMethodChange(fuelCalculationMethod) {
         
         isDistanceSet = true;
         $("#distance_form3").each(function(){ $(this).hide(); });
-        $("#currency_div_form2").css("display", "none");
-        $("#distance_div_form2").css("display", "block");
+        $("#currency_div_form2").slideUp("slow");
+        $("#distance_div_form2").slideDown("slow");
 
         carToJob(false);
         driveToJob(false);
@@ -354,15 +377,15 @@ function fuelCalculationMethodChange(fuelCalculationMethod) {
     }
 }
 
-//function that for the radio button with the question 
+//function for the radio button with the question 
 //'Considering you drive to work?' in the Fuel section of Form part 2
 function carToJob(carToJobFlag) {
     if (carToJobFlag) {
         //selects actively radio button to which this function is associated
         $("#car_job_form2_yes").prop("checked", true); 
         
-        $("#div_car_job_yes_form2").css("display", "block");
-        $("#div_car_job_no_form2").css("display", "none");
+        $("#div_car_job_yes_form2").slideDown("slow");
+        $("#div_car_job_no_form2").slideUp("slow");
         $(".time_spent_part1_form3").each(function(){ $(this).show(); });
         $(".time_spent_part2_form3").hide();
         
@@ -374,8 +397,8 @@ function carToJob(carToJobFlag) {
         //selects actively radio button to which this function is associated
         $("#car_job_form2_no").prop("checked", true); 
         
-        $("#div_car_job_yes_form2").css("display", "none");
-        $("#div_car_job_no_form2").css("display", "block");
+        $("#div_car_job_yes_form2").slideUp("slow");
+        $("#div_car_job_no_form2").slideDown("slow");
         $(".time_spent_part1_form3").each(function(){ $(this).hide(); });
         $(".time_spent_part2_form3").show();
         
@@ -393,50 +416,69 @@ function carToJob(carToJobFlag) {
 function driveToJob(flag){
     if(flag){
         //selects actively radio button to which this function is associated
-        $("#drive_to_work_yes_form3").prop("checked", true);
+        //$("#drive_to_work_yes_form3").prop("checked", true);
         
-        $(".car_to_job_distance_form3").each(function(){ $(this).show(); });
-        $(".time_spent_part1_form3").each(function(){ $(this).show(); });
-        $(".time_spent_part2_form3").hide();
-        $("#car_no_job_distance_form3").hide();
+        //distance section
+        $("#car_no_job_distance_form3").fadeOut(function(){
+            $(".car_to_job_distance_form3").each(function(i, elm){ 
+                $(elm).fadeIn("slow"); 
+            });           
+        });
 
         //set to "no" the question "Do you have a job or a worthy occupation?" 
         //in Working Time section of Form Part 3         
         working_time_toggle(true);
-        $("#working_time_part1_form3").hide();
-        $("#working_time_part2_form3").show();
+        $("#working_time_part1_form3").hide("slow");
+        $("#working_time_part2_form3").show("slow");
+        
+        //time spent in driving section
+        $(".time_spent_part2_form3").fadeOut("slow", function(){
+            $(".time_spent_part1_form3").each(function(i, elm){ 
+                $(elm).fadeIn("slow"); 
+            });            
+        });
+      
     }
     else{
         //selects actively radio button to which this function is associated
         $("#drive_to_work_no_form3").prop("checked", true);
         
-        $(".car_to_job_distance_form3").each(function(){ $(this).hide(); });
-        $(".time_spent_part1_form3").each(function(){ $(this).hide(); });
-        $(".time_spent_part2_form3").show();
-        $("#car_no_job_distance_form3").show();
+        $(".car_to_job_distance_form3").each(function(i, elm){
+            $(elm).fadeOut("slow")
+         }).promise().done( function(){ 
+            $("#car_no_job_distance_form3").fadeIn("slow");
+        });
         
+        //in Working Time section of Form Part 3  
         working_time_toggle(true);
-        $("#working_time_part1_form3").show();
-        $("#working_time_part2_form3").hide();
+        $("#working_time_part1_form3").show("slow");
+        $("#working_time_part2_form3").hide("slow");
+        
+        //time spent in driving section
+        $(".time_spent_part1_form3").each(function(i, elm){
+            $(elm).fadeOut("slow");
+        }).promise().done( function(){ 
+            $(".time_spent_part2_form3").fadeIn("slow");
+        });       
     }
 }
 
 function tolls_daily(tollsDailyFlag) {
     if (tollsDailyFlag) {
-        $("#dia_nao_portag_DIV").css("display", "none");
-        $("#dia_sim_portag_DIV").css("display", "block");
+        $("#dia_nao_portag_DIV").slideUp("slow");
+        $("#dia_sim_portag_DIV").slideDown("slow");
     } else {
-        $("#dia_nao_portag_DIV").css("display", "block");
-        $("#dia_sim_portag_DIV").css("display", "none");
+        $("#dia_nao_portag_DIV").slideDown("slow");
+        $("#dia_sim_portag_DIV").slideUp("slow");
     }
 }
 
 /*function that toggles some div between visible or hidden*/
 function onclick_div_show(divID, flag) {
     if(flag) {
-        $(divID).css("display", "block");
+        $(divID).show("slow");
     } else {
-        $(divID).css("display", "none");
+        $(divID).hide("slow");
     }
 }
 
@@ -447,39 +489,41 @@ function slider_toggles_form3(){
     var ckb2 = $("#slider2").is(":checked");
 
     if(ckb1){
-        $("#public_transp_Div_form3").css("display", "block");
+        $("#public_transp_Div_form3").show("slow");
     }
     else{
-        $("#public_transp_Div_form3").css("display", "none");
+        $("#public_transp_Div_form3").hide("slow");
     }
 
     if(ckb2){
-        $("#fin_effort_Div_form3").css("display", "block");
+        $("#fin_effort_Div_form3").show("slow");
     }
     else{
-        $("#fin_effort_Div_form3").css("display", "none");
+        $("#fin_effort_Div_form3").hide("slow");
     }
 
     //if Public Transporst or Financial Effort toogle sliders in form part 3 are activated,
     //shows Distance and Time spent in driving form part 3 section
     if(ckb1 || ckb2){
-        $("#distance_time_spent_driving_form3").css("display", "block");
+        $("#distance_time_spent_driving_form3").show("slow");
     }
     else{
-        $("#distance_time_spent_driving_form3").css("display", "none");
+        $("#distance_time_spent_driving_form3").hide("slow");
     }
 }
 
+/*Some Jquery Document functions*/
+
+//sliders in form part 3
 $("#slider1").change(function() {
     slider_toggles_form3();
 });
-
 $("#slider2").change(function() {
     slider_toggles_form3();
 });
 
 //fade out lateral and top divs when mouse over central main div
-$('#input_div').hover(
+$('#form_part1').hover(
     function(){//when mouse pointer enters div
         $('#description, #div1_td, #div3_td').removeClass('no_fade').addClass('fade_out');
         scrollPage();
@@ -573,23 +617,23 @@ function numberWithSpaces(x) {
 function income_toggle(value){
     switch(value){
         case "year":
-            $("#income_per_year_form3, #working_time_form3").removeClass("hidden").show();
-            $("#income_per_month_form3, #income_per_week_form3, #income_per_hour_form3").addClass("hidden");
+            $("#income_per_year_form3, #working_time_form3").show("slow");
+            $("#income_per_month_form3, #income_per_week_form3, #income_per_hour_form3").hide("slow");
             income="year";
             break;
         case "month":
-            $("#income_per_month_form3, #working_time_form3").removeClass("hidden").show();
-            $("#income_per_year_form3, #income_per_week_form3, #income_per_hour_form3").addClass("hidden");
+            $("#income_per_month_form3, #working_time_form3").show("slow");
+            $("#income_per_year_form3, #income_per_week_form3, #income_per_hour_form3").hide("slow");
             income="month";
             break;
         case "week":
-            $("#income_per_week_form3, #working_time_form3").removeClass("hidden").show();
-            $("#income_per_year_form3, #income_per_month_form3, #income_per_hour_form3").addClass("hidden");
+            $("#income_per_week_form3, #working_time_form3").show("slow");
+            $("#income_per_year_form3, #income_per_month_form3, #income_per_hour_form3").hide("slow");
             income="week";
             break;
         case "hour":
-            $("#income_per_hour_form3").removeClass("hidden").show();
-            $("#income_per_year_form3, #income_per_week_form3, #income_per_month_form3, #working_time_form3").addClass("hidden");
+            $("#income_per_hour_form3").show("slow");
+            $("#income_per_year_form3, #income_per_week_form3, #income_per_month_form3, #working_time_form3").hide("slow");
             income="hour";
             break;
     }   
@@ -599,15 +643,13 @@ function income_toggle(value){
 function working_time_toggle(value){
     if(value){
         //selects actively radio button to which this function is associated
-        $("#working_time_yes_form3").prop("checked", true);
-        
-        $("#working_time_input_form3").show();
+        $("#working_time_yes_form3").prop("checked", true);       
+        $("#working_time_input_form3").show("slow");
     }
     else{
         //selects actively radio button to which this function is associated
         $("#working_time_no_form3").prop("checked", true);
-        
-        $("#working_time_input_form3").hide();
+        $("#working_time_input_form3").hide("slow");
     }  
 }
 
