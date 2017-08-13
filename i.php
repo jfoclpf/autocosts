251c525773b1cdd09c5d5fe22ee19615baef9f3c
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2017-08-13 22:03:01 +0200

    update

--- i.php
+++ i.php
@@ -2,6 +2,28 @@
 include_once("./php/functions.php");
 include_once("./countries/_list.php");
 include_once("./countries/_url_selector.php");
+function sanitize_output($buffer) {
+
+    $search = array(
+        '/\>[^\S ]+/s',     // strip whitespaces after tags, except space
+        '/[^\S ]+\</s',     // strip whitespaces before tags, except space
+        '/(\s)+/s',         // shorten multiple whitespace sequences
+        '/<!--(.|\s)*?-->/' // Remove HTML comments
+    );
+
+    $replace = array(
+        '>',
+        '<',
+        '\\1',
+        ''
+    );
+
+    $buffer = preg_replace($search, $replace, $buffer);
+
+    return $buffer;
+}
+
+ob_start("sanitize_output");
 ?><!DOCTYPE html>
 
 <html lang="<?php echo HTML_tag_lang($language, $GLOBALS['country']); ?>">
