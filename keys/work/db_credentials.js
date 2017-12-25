5d2e01a97c146f1f159a9439b228ca29a19570b0
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2017-12-25 22:47:49 +0000

    update

--- keys/work/db_credentials.js
+++
@@ -1,9 +0,0 @@
-function get_DBcredentials(){
-    var login_UserInputDB = {
-      host     : '127.0.0.1',
-      user     : 'jfolpf_dev',
-      password : '124devAC%',
-      database : 'jfolpf_autocosts_test'
-    };
-    return login_UserInputDB;
-}
