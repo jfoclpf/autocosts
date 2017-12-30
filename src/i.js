70d4ef17f9a6bcbb8fe8ea96a3fea5f93269d1c1
jfoclpf <joao.pimentel.ferreira@gmail.com>
2017-12-30 19:15:30 +0000

    node express imrpoved

--- src/i.js
+++ src/i.js
@@ -5,27 +5,40 @@
 **                                             **
 ************************************************/
 
-const $ = require('jQuery');
+var Country;
+
+const $ = require('jquery');
+const fs = require('fs');
 const express = require('express');
-const Handlebars = require('handlebars');
+const exphbs  = require('express-handlebars');
+
 var app = express();
 
-var source = "<p>Hello, my name is {{name}}. I am from {{hometown}}. I have " +
-             "{{kids.length}} kids:</p>" +
-             "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>";
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-var template = Handlebars.compile(source);
- 
-var data = { "name": "Alan", "hometown": "Somewhere, TX",
-             "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
+app.use(express.static(__dirname + '/public'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/images', express.static(__dirname + '/images'));
+app.use('/js', express.static(__dirname + '/js'));
 
-var result = template(data);
-
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/:Country', function (req, res) {
-  res.send(result);
+//CC stands for 2-letter Country Code
+app.get('/:CC', function (req, res) {
+	var CC = req.params.CC;
+	
+	var list = JSON.parse(fs.readFileSync(__dirname + '/countries/list.json', 'utf8'));	
+	var words = JSON.parse(fs.readFileSync(__dirname + '/countries/' + CC + '.json', 'utf8'));
+	
+	var lang_code = list.languages_CT[CC];
+	console.log(lang_code);
+	
+	var data = {"title" : words.main_title , "main_title" : words.main_title, "initial_text": words.initial_text}
+	
+	res.render('home', data)
 })
 
-app.listen(3004, function () {
-  console.log('Example app listening on port 3004!');
+
+var HTTPport = 3020; 
+app.listen(HTTPport, function () {
+  console.log('Listening on port ' + HTTPport);
 });
