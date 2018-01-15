d5f4b0f9e4c2ecbeb73d448672109274210ece59
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2018-01-15 20:57:42 +0100

    Globals.js file will be rendered with handlebars and not ejs

--- src/server/Globals.js
+++ src/server/Globals.js
@@ -3,13 +3,45 @@ module.exports = function(req, res, dataObj) {
     console.log("\nRoute: app.get('/Globals.js')");       
     
     res.set('Content-Type', 'application/javascript');
-    res.render('Globals.ejs', { 
-        "CC" : dataObj.CC,
-        "LangCode" : dataObj.LangCode,
+    
+    var CC = req.params.CC;
+    var protocol = getProtocol(req, dataObj.IS_HTTPS);
+    
+    var data = {
+        "CC" : CC,
+        "LangCode" : dataObj.languages_CT[CC],
         "available_CT" : dataObj.available_CT,
         "domains_CT" : dataObj.domains_CT,
         "clientDir" : dataObj.clientDir,
         "CDN_URL" : dataObj.CDN_URL,
-        "HTTP_Protocol" : dataObj.HTTP_Protocol
-    });
+        "HTTP_Protocol" : protocol,
+        "layout": false
+    };
+    
+    console.log("data to render", data);
+    
+    res.render('Globals', data);
 }
+
+
+var getProtocol = function (req, IS_HTTPS){
+    
+    var host = req.get('host');
+    
+    if (IS_HTTPS && !isWorkDomain(req)){
+        return "https";
+    }
+    return "http";
+};
+
+var isWorkDomain = function (req){
+
+    var host = req.get('host');
+    var hostSplit = host.split(".");
+    var tld = hostSplit[hostSplit.length-1];
+    
+    if (tld.toLowerCase() === "work"){
+        return true;
+    }
+    return false;
+};
