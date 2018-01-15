e3360f44ae2539c4161c41df35bc5ba76f670b50
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2018-01-15 20:53:35 +0100

    update node version to run on remote machine

--- rsync2work.sh
+++ rsync2work.sh
@@ -3,9 +3,10 @@ cd "$(dirname "$0")"
 
 rsync -avz --delete --exclude '.git*' --exclude 'rsync2work.sh' --exclude 'app/' --exclude 'build/' . jfolpf@autocosts.work:/var/www/autocosts.work/
 
-ssh jfolpf@autocosts.work 'pkill -f /usr/bin/node'
+ssh jfolpf@autocosts.work 'pkill -f /usr/local/bin/node'
 
-ssh jfolpf@autocosts.work 'cd /var/www/autocosts.work/src && /usr/bin/node index.js'
+ssh jfolpf@autocosts.work 'cd /var/www/autocosts.work/src && /usr/local/bin/node index.js'
+
 
 
 
