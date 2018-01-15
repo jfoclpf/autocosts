c78f900acacae0c1815f5a66211bc37e3ead485c
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2018-01-15 20:57:42 +0100

    Globals.js file will be rendered with handlebars and not ejs

--- src/index.js
+++ src/index.js
@@ -26,16 +26,16 @@ const fs = require('fs');
 const path  = require("path");
 const express = require('express');
 const exphbs  = require('express-handlebars');
-const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const compression = require('compression');
+const request = require('request');
 
 //personalised requires
 const url = require(__dirname + '/server/url'); //to deal with the full URL rules and redirect accordingly
 const submitUserInput = require(__dirname + '/server/submitUserInput');
 const Globals = require(__dirname + '/server/Globals');
 
-const clientDir = 'client/';
+const clientDir = 'client/'; //directory with respect to root public HTML, where the client JS flies will be stored
 const HOME_DIR = path.resolve(__dirname, '..') + "/"; //parent directory of current file directory
 const SRC_DIR = HOME_DIR + "src" + "/";
 
@@ -58,26 +58,34 @@ else{
 console.log("chosen '" + REL + "'");
 //process.exit();
 
-//include credentials object
-var DB_INFO = JSON.parse(fs.readFileSync(HOME_DIR + 'keys/' + REL + '/db_credentials.json'));
-console.log(DB_INFO);
+//selects CDN_URL Global variable, in case the CDN is used
+var CDN_URL;
+if(IS_CDN){
+    if(url.isThisATest(req)){
+        CDN_URL = CDN_URL_WORK;
+    }
+    else{
+        CDN_URL = CDN_URL_PROD;
+    }
+}
+else{
+    CDN_URL = "";
+}
+console.log("CDN_URL: " + CDN_URL);
 
-const CountriesInfo = JSON.parse(fs.readFileSync(__dirname + '/countries/list.json', 'utf8'));	
-const available_CT = CountriesInfo.available_CT; //available Countries
-const languages_CT = CountriesInfo.languages_CT; //Language Codes
-const domains_CT   = CountriesInfo.domains_CT;   //Domains
-
-/*Global Variables*/
-//merely defaults, they may change later accordingly
-var GLOB_VAR = {
-    "CC"            : DefaultCC, //CC stands for 2-letter Country Code
-    "available_CT"  : available_CT,
-    "domains_CT"    : domains_CT,
-    "clientDir"     : clientDir,
-    "LangCode"      : languages_CT[DefaultCC],
-    "CDN_URL"       : "",
-    "HTTP_Protocol" : "http"
+//fixed unchangeable global data which is constant for all HTTP requests independently of the country
+const CountriesInfo = JSON.parse(fs.readFileSync(__dirname + '/countries/list.json', 'utf8'));
+const GlobData = {    
+    "available_CT"  : CountriesInfo.available_CT, //available Countries
+    "languages_CT"  : CountriesInfo.languages_CT, //Language Codes
+    "domains_CT"    : CountriesInfo.domains_CT,   //Domains
+    "CDN_URL"       : CDN_URL,
+    "DBInfo"        : JSON.parse(fs.readFileSync(HOME_DIR + 'keys/' + REL + '/db_credentials.json')), //include credentials object    
+    "IS_HTTPS"      : IS_HTTPS,  //changed on the top of the code
+    "DefaultCC"     : DefaultCC, //changed on the top of the code
+    "clientDir"     : clientDir  //directory with respect to root public HTML, where the client JS flies will be stored
 };
+//console.log("Global Constant Object", GlobData);
 
 var app = express();
 app.enable('case sensitive routing');
@@ -88,25 +96,27 @@ var hbs = exphbs.create({
     extname: '.hbs',
     helpers: {
         /*using for selecting value in HTML select boxes*/
-        isSelected: function (value) {
-            return GLOB_VAR.CC === value ? 'selected' : ''; 
+        isSelected: function (CC, value) {
+            return CC === value ? 'selected' : ''; 
         },
         /*chose the HTML costs table for specific country*/
-        costs_table: function (){
-            return GLOB_VAR.CC+'costs';
+        costs_table: function (CC){
+            return CC+'costs';
         },
-        banner_flag: function (){
-            return GLOB_VAR.CC.toLowerCase() + ' ' + 'flag';
-        } 
+        banner_flag: function (CC){
+            return CC.toLowerCase() + ' ' + 'flag';
+        },
+        json: function(context) {
+            return JSON.stringify(context);
+        }
     }
 });
-app.engine('.hbs', hbs.engine);
 
-//rendering engine for dynamically loaded JS files
-app.engine('.js', ejs.__express);
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
 
 //static content
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')); //root public folder
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/images', express.static(__dirname + '/images'));
 app.use('/client', express.static(__dirname + '/client'));
@@ -116,34 +126,11 @@ app.use(compression());
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-//checks CDN
-app.use(function (req, res, next) {
-    if(IS_CDN){
-        if(url.isThisATest(req)){
-            CDN_URL = CDN_URL_WORK;
-        }
-        else{
-            CDN_URL = CDN_URL_PROD;
-        }
-    }
-    else{
-        CDN_URL = "";
-    }
-    console.log("CDN_URL: " + CDN_URL);
-    next();
-});
-
-//Javavascript Globals.js file to the client with variables inserted by the server
-//The order of app.get must be preserved
-app.set('view engine', '.js');
-app.get('/Globals.js', function(req, res){    
-    Globals(req, res, GLOB_VAR);
-});
 
 /* This detects the region of the user by his locale and tries to return,
 if applicable, a cost per unit distance of the correspondent 
 UBER service in that region from which the user is accessing the site */
-app.get('/get_uber', function(req, res) {
+app.get('/getUBER', function(req, res) {
     
     if (!url.isThisLocalhost(req)){
         //tries to get IP from user
@@ -173,9 +160,26 @@ app.get('/get_uber', function(req, res) {
         //to manage tokens, visit: https://developer.uber.com/dashboard
         //user:info@autocosts.info | pass: V*************
         var uber_token = JSON.parse(fs.readFileSync(HOME_DIR + 'keys/' + REL + '/uber_token.json'));
+        console.log("uber_token", uber_token);
         var uber_API_url = "https://api.uber.com/v1.2/products?latitude=" + 
-                            lat + "&longitude=" + long + "&server_token=" + uber_token;
-        console.log(uber_API_url);
+                            lat + "&longitude=" + long + "&server_token=" + uber_token;        
+        console.log("uber_API_url", uber_API_url);
+        
+        //HTTP Header reques
+        var options = {
+            url: uber_API_url,
+            headers: {
+                'Accept-Language': 'en_US',
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+        };        
+
+        request(options, function(error, response, body){
+            if (!error && response.statusCode == 200) {
+            var info = JSON.parse(body);
+                console.log("Uber response: ", info);
+            }        
+        });        
     }
     
 });
@@ -183,48 +187,51 @@ app.get('/get_uber', function(req, res) {
 //Javavascript Globals.js file to the client with variables inserted by the server
 //The order of app.get must be preserved
 app.post('/submitUserInput', function(req, res) {              
-    submitUserInput(req, res, DB_INFO);
+    submitUserInput(req, res, GlobData);
 });
 
-//Routing for HTML layout and forms
-app.set('view engine', '.hbs');
+app.get('/Globals.js/:CC', function(req, res){
+    Globals(req, res, GlobData);
+});
+
 app.get('/:CC', function (req, res, next) {
     console.log("\nRoute: app.get('/CC')");
-    
+           
     //returns true if it was redirected
-    var wasRedirected = url.getCC(req, res, GLOB_VAR);
+    var wasRedirected = url.getCC(req, res, GlobData);
     if(wasRedirected){
         return;
     };
-    
-    //Global object set
-    GLOB_VAR.CC = req.params.CC;
-    GLOB_VAR.LangCode = languages_CT[GLOB_VAR.CC]; //language codes
-    GLOB_VAR.HTTP_Protocol = url.getProtocol(req, IS_HTTPS);        
+        
+    var CC = req.params.CC;
+    var LangCode = GlobData.languages_CT[CC]; //language codes
+    var HTTP_Protocol = url.getProtocol(req, GlobData.IS_HTTPS);        
 
-    console.log("Country code: "  + GLOB_VAR.CC);
-    console.log("Language code: " + GLOB_VAR.LangCode);
+    console.log("Country code: "  + CC);
+    console.log("Language code: " + LangCode);
     
-    var words = JSON.parse(fs.readFileSync(__dirname + '/countries/' + GLOB_VAR.CC + '.json', 'utf8'));
-    words.word_per += "&#32;" //add non-breaking space    
+    //data to be rendered embedded in the HTML file
+    var data = JSON.parse(fs.readFileSync(__dirname + '/countries/' + CC + '.json', 'utf8'));
+    data.CC = CC; //add new property
+    data.word_per += "&#32;" //add non-breaking space 
 
     //add country_select (list of countries) to object to be rendered
     //to render the dropdown countries list box
-    words.countriesDropDownList = CountriesInfo.available_CT;
-    delete words.countriesDropDownList["XX"];
+    data.countriesDropDownList = CountriesInfo.available_CT;
+    delete data.countriesDropDownList["XX"];
 
-    res.render('home', words);
+    res.render('home', data);
 });
 
 app.get('/', function (req, res, next) {
     console.log("\nRoute: app.get('/')");
-    url.redirect(req, res, GLOB_VAR);    
+    url.redirect(req, res, GlobData);    
 });
 
 //error handler
 app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 })
 
 var HTTPport = 3000; 
