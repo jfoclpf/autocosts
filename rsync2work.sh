574abd7570c9836781b3585219a19d76c77f86a6
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2018-01-20 21:12:59 +0100

    allows to build on remote machine and then run index.js

--- rsync2work.sh
+++ rsync2work.sh
@@ -5,7 +5,19 @@ rsync -avz --delete --exclude '.git*' --exclude 'rsync2work.sh' --exclude 'app/'
 
 ssh jfolpf@autocosts.work 'pkill -f /usr/local/bin/node'
 
-ssh jfolpf@autocosts.work 'cd /var/www/autocosts.work/src && /usr/local/bin/node index.js'
+#when no arguments, just runs using src/ folder
+if [ $# -eq 0 ] 
+then
+    ssh jfolpf@autocosts.work 'cd /var/www/autocosts.work/src && /usr/local/bin/node index.js'
+fi
+
+#builds (compressing all the code, etc.) and runs on the build/ folder
+if [ $1 = "build" ] 
+then
+    ssh jfolpf@autocosts.work 'cd /var/www/autocosts.work && ./build.sh -cesrtim'
+    ssh jfolpf@autocosts.work 'cd /var/www/autocosts.work/build && /usr/local/bin/node index.js'
+fi
+
 
 
 
