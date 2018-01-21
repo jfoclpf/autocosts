d29d8f801110412d463a7ee703c9812fad959fe5
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2018-01-21 19:37:50 +0100

    sort dropdown countries box alphabetically

--- src/server/getCC.js
+++ src/server/getCC.js
@@ -1,5 +1,6 @@
 
 const fs = require('fs');
+const sortObj  = require('sort-object'); //to sort JS objects
 const url = require(__dirname + '/url');
 
 module.exports = function(req, res, GlobData) {
@@ -27,7 +28,7 @@ module.exports = function(req, res, GlobData) {
     
     //add country_select (list of countries) to object to be rendered
     //to render the dropdown countries list box
-    data.countriesDropDownList = GlobData.available_CT;
+    data.countriesDropDownList = sortObj(GlobData.available_CT);
     delete data.countriesDropDownList["XX"];
     
     //add domains array
