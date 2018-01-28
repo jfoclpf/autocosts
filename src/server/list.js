178c37a40a3fdc344b23f348ee9c4ebd355a0d3e
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2018-01-28 12:24:38 +0100

    clone copy WORDS on the list/ to avoid removing the XX option on Global WORDS when /list is loaded

--- src/server/list.js
+++ src/server/list.js
@@ -2,7 +2,7 @@
 module.exports = function(req, res, GlobData, WORDS) {
 
     var data = {};
-    data.WORDS = WORDS;
+    data.WORDS = JSON.parse(JSON.stringify(WORDS)); //clone object
     delete data.WORDS["XX"];
     data.layout = false;
 
