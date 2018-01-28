66963aa9239566902dba05430205e10a3643d287
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2018-01-28 11:22:15 +0100

    improved /list of costs

--- src/server/list.js
+++ src/server/list.js
@@ -3,6 +3,7 @@ module.exports = function(req, res, GlobData, WORDS) {
 
     var data = {};
     data.WORDS = WORDS;
+    delete data.WORDS["XX"];
     data.layout = false;
 
     var fileToRender = GlobData.INDEX_DIR + "views/list.hbs";
