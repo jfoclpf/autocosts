0406d2653c358ac1ca1339b7fd20d24a8aa4abda
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2018-04-29 19:04:46 +0200

    google analytics js file loading improved

--- src/client/initialize.js
+++ src/client/initialize.js
@@ -26,26 +26,18 @@
 
     /*Google Analytics*/
     if(navigator.userAgent.indexOf("Speed Insights") == -1 && !IsThisAtest() && SWITCHES.g_analytics) {
-        (function(i, s, o, g, r, a, m) {
-            i.GoogleAnalyticsObject = r;
-            i[r] = i[r] || function() {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
-            a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0];
-            a.async = 1;
-            a.src = g;
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', JS_FILES.Google.analytics, 'ga');
-
-        //change according to your site
-        ga('create', GA_TRACKING_ID, 'auto');
-        ga('set', 'displayFeaturesTask', null);
-        ga('send', 'pageview');
         
-        //detects whether Google Analytics has loaded
-        //tries every second
-        check_ga(1000);
+        getScriptOnce(JS_FILES.Google.analytics, function(){
+            window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;                                
+            //change according to your site
+            ga('create', GA_TRACKING_ID, 'auto');
+            ga('set', 'displayFeaturesTask', null);
+            ga('send', 'pageview');        
+        
+            //detects whether Google Analytics has loaded
+            //tries every second
+            check_ga(1000);
+        });                
     }
     
 })();
