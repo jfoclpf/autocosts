aa670c5a3d4f29c68c902253feeda4c145655c22
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2019-01-14 02:46:48 +0100

    add css to /list

--- src/server/list.js
+++ src/server/list.js
@@ -1,10 +1,28 @@
 const path  = require('path');
+const url   = require(path.join(__dirname, 'url')); //own project module
 
 module.exports = function(req, res, serverData, words) {
 
     var data = {};
-    data.words = JSON.parse(JSON.stringify(words)); //clone object
-    delete data.words.XX;
+    
+    data.isList = true;
+        
+    data.wordsMatrix = JSON.parse(JSON.stringify(words)); //clone object
+    delete data.wordsMatrix.XX;
+    
+    data.words = data.wordsMatrix.UK;
+    
+    //information depending on this request from the client    
+    var clientData = {
+        "fullURL"      : url.fullUrl(req),              //full url, ex: "https://autocosts.info/stats"
+        "basicURL"     : url.basicURL(req),             //basic url, ex: "https://autocosts.info"
+        "languageCode" : "en",                      //this page of World Statistics of car, renders only in English             
+        "isThisATest"  : url.isThisATest(req),      //boolean variable regarding if present request is a test
+        "notLocalhost" : !url.isThisLocalhost(req), //boolean variable regarding if present request is from localhost
+        "httpProtocol" : url.getProtocol(req, serverData.settings.switches.https)
+    }    
+    data.clientData = clientData;    
+        
     data.layout = false;
 
     var fileToRender = path.join(serverData.directories.index, "views", "list.hbs");
