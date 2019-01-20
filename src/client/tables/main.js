26c38925272dab9610021c27499cb929bb234c8c
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2019-01-20 18:00:19 +0100

    externalize js file in stats tables

---
+++ src/client/tables/main.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+
+    var globalVariables = document.getElementById('global_variables');
+    var countryCode = globalVariables.dataset.country_code;
+    console.log("Loaded stats page for: " + countryCode);
+
+    $("#countrySelectStats").val(countryCode);
+
+    $('#countrySelectStats').on('change', function() {
+        //forward to the respective page, from ..../AA.htm to ..../BB.htm
+        window.location.href = window.location.origin + "/tables/" + this.value + ".htm";
+    });
+
+    //download Table Button
+    $(".downloadButton").on("click", function(e){
+        e.preventDefault();  //stop the browser from following
+        //forward to the respective page, from ..../AA.htm to ..../AA.jpg
+        var urlToGo = window.location.origin + "/tables/" + countryCode + ".jpg";
+        var win = window.open(urlToGo, '_blank');
+        win.focus();
+    });
+});
