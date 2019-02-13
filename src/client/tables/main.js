ae98f68f82e2b170d83c6becaf6edf56ca690164
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2019-02-13 22:29:15 +0100

    standard JS compliant

--- src/client/tables/main.js
+++ src/client/tables/main.js
@@ -1,23 +1,24 @@
-$( document ).ready(function() {
+/* globals $ */
 
-    var globalVariables = document.getElementById('global_variables');
-    var countryCode = globalVariables.dataset.country_code;
-    console.log("Loaded stats page for: " + countryCode);
+$(document).ready(function () {
+  var globalVariables = document.getElementById('global_variables')
+  var countryCode = globalVariables.dataset.country_code
+  console.log('Loaded stats page for: ' + countryCode)
 
-    $("#countrySelectStats").val(countryCode);
+  $('#countrySelectStats').val(countryCode)
 
-    $('#countrySelectStats').on('change', function() {        
-        window.location.href = window.location.origin + "/tables/" + this.value + ".htm";
-    });
+  $('#countrySelectStats').on('change', function () {
+    window.location.href = window.location.origin + '/tables/' + this.value + '.htm'
+  })
 
-    //download Table Button
-    $(".downloadButton").on("click", function(e){
-        e.preventDefault();  //stop the browser from following
-        //forward to the respective page, from ..../AA.htm to ..../AA.jpg
-        var urlToGo = window.location.origin + "/tables/" + countryCode + ".jpg";
-        var win = window.open(urlToGo, '_blank');
-        win.focus();
-    });
-    
-    $(".main-title a").attr("href","/" + countryCode);
-});
+  // download Table Button
+  $('.downloadButton').on('click', function (e) {
+    e.preventDefault() // stop the browser from following
+    // forward to the respective page, from ..../AA.htm to ..../AA.jpg
+    var urlToGo = window.location.origin + '/tables/' + countryCode + '.jpg'
+    var win = window.open(urlToGo, '_blank')
+    win.focus()
+  })
+
+  $('.main-title a').attr('href', '/' + countryCode)
+})
