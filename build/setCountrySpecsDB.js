6e1ab53b05a9521c9e1216d172041546a02cadc7
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2019-02-28 22:59:25 +0100

    better understanding of async and error management

--- build/setCountrySpecsDB.js
+++ build/setCountrySpecsDB.js
@@ -1,4 +1,6 @@
-/* script that populates a country DB with specs for each country */
+/* script that populates a country database with specifications for each country
+   namely the standard distance (km, miles, etc.), fuel amount (ltr, gal, etc.) and
+   fuel efficiency (l/100km, km/l, mpg(imp), etc.) */
 
 console.log("Populating the 'countries specs DB' with information from the countries files")
 console.log('\nRunning script ', __filename, '\n')
@@ -14,19 +16,25 @@ const colors = require('colors') // eslint-disable-line
 
 const commons = require(path.join(__dirname, '..', 'commons'))
 
+// database variables
+var db, DB_INFO
+
+// Object
+var availableCountries
+
 commons.init()
 // Main directories got from commons
 var directories = commons.getDirectories()
 var settings = commons.getSettings()
 
 // checks for internet connection
-isOnline().then(online => {
+isOnline().then(function (online) {
   if (!online) {
     console.log('ERROR: no Internet connection'.red.bold)
     process.exit(1) // exit with error
   }
 
-  var DB_INFO = settings.dataBase.credentials
+  DB_INFO = settings.dataBase.credentials
   // detect for null or empty object
   if (!DB_INFO || Object.keys(DB_INFO).length === 0) {
     throw commons.getDataBaseErrMsg(__filename, settings.dataBase)
@@ -39,84 +47,118 @@ isOnline().then(online => {
   // getting country information from
   console.log('\nGet Countries info from: ' + fileNames.project.countriesListFile)
   var countriesInfo = JSON.parse(fs.readFileSync(fileNames.project.countriesListFile, 'utf8'))
-
-  var availableCountries = countriesInfo.availableCountries
+  availableCountries = countriesInfo.availableCountries
 
   // sorts array of countries
   availableCountries = sortObj(availableCountries)
   delete availableCountries.XX
 
-  // process.exit();
-
-  // database variable
-  var db
-
-  async.series([
-
-    // delete table before populate it
-    function (callback) {
-      console.log('Deleting DB table ' + DB_INFO.database + '->' + DB_INFO.db_tables.country_specs +
-                        ' ' + 'before populating it with country specs')
-
-      db = mysql.createConnection(DB_INFO)
-
-      db.connect(function (err) {
-        if (err) {
-          console.error('error connecting: ' + err.stack)
-          return
-        }
-        console.log('User ' + DB_INFO.user +
-                            ' connected successfully to DB ' + DB_INFO.database +
-                            ' at ' + DB_INFO.host)
-      })
-
-      db.query('DELETE FROM ' + DB_INFO.db_tables.country_specs, function (err, results, fields) {
-        if (err) { console.log(err); throw err }
-        // console.log(countries);
-        console.log('Previous data deleted from table')
-        callback()
-      })
-    },
-
-    function (callback) {
-      var queryInsert
-      for (var key in availableCountries) {
-        let WORDS = JSON.parse(fs.readFileSync(path.join(directories.src.countries, key + '.json'), 'utf8'))
-        queryInsert = 'INSERT INTO ' + DB_INFO.db_tables.country_specs + ' ( ' +
-                    'Country, ' +
-                    'currency, ' +
-                    'distance_std, ' +
-                    'fuel_efficiency_std, ' +
-                    'fuel_price_volume_std ' +
-                    ') ' +
-                    'VALUES ( ' +
-                    '"' + key + '", ' +
-                    '"' + WORDS.curr_code + '", ' +
-                    WORDS.distance_std_option + ', ' +
-                    WORDS.fuel_efficiency_std_option + ', ' +
-                    WORDS.fuel_price_volume_std +
-                    ')'
-
-        var size = Object.keys(availableCountries).length
-        var i = 0
-        db.query(queryInsert,
-          (function (key2) {
-            return function (err, results, fields) {
-              if (err) { console.log(err); throw err }
-              process.stdout.write(key2 + ' ')
-              i++
-              // last callback
-              if (i === size) {
-                process.stdout.write('\n' + 'Countries specs inserted into respective DB\n\n')
-                db.end()
-                callback()
-              }
-            }
-          })(key))
+  async.series([dbConnects, deletesTable, populatesTable, dbEnd],
+    function (err, results) {
+      if (err) {
+        console.log(err.message)
+        process.exit(1)
       }
+      console.log('Countries specifications and standards inserted successfully into respective database'.green)
+      process.exit(0) // exit successfully
     }
-  ])
+  )
+  // end of async
+  // if not online
 }).catch(function (err) {
   console.log(err)
-  process.exit(1)// with error
+  process.exit(1) // with error
 })
+
+// main function of async.series([dbConnects, deletesTable, populatesTable]
+function dbConnects (next) {
+  db = mysql.createConnection(DB_INFO)
+  db.connect(function (err) {
+    if (err) {
+      next(Error('Error connecting to database: ' + err.message))
+    }
+    console.log('User ' + DB_INFO.user +
+                        ' connected successfully to DB ' + DB_INFO.database +
+                        ' at ' + DB_INFO.host)
+    next()
+  })
+}
+
+// main function of async.series([dbConnects, deletesTable, populatesTable, dbEnd]
+// delete table before populate it
+function deletesTable (next) {
+  console.log('Deleting DB table ' + DB_INFO.database + '->' + DB_INFO.db_tables.country_specs +
+                    ' ' + 'before populating it with country specs')
+
+  db.query('DELETE FROM ' + DB_INFO.db_tables.country_specs, function (err, results, fields) {
+    if (err) {
+      next(Error('Error on db query' + err.message))
+    }
+    // console.log(countries);
+    console.log('Previous data deleted from table')
+    next()
+  })
+}
+
+// main function of async.series([dbConnects, deletesTable, populatesTable, dbEnd]
+function populatesTable (next) {
+  var countryCodesArray = Object.keys(availableCountries) // ['PT', 'US', 'AU', etc.]
+  var numberOfCountries = countryCodesArray.length
+
+  var createFunctionForAsync = function (CC) {
+    return function (callback) {
+      queryForCC(CC, callback)
+    }
+  }
+
+  var functionsArray = []
+  for (let i = 0; i < numberOfCountries; i++) {
+    let CC = countryCodesArray[i]
+    functionsArray.push(createFunctionForAsync(CC))
+  }
+
+  async.parallel(functionsArray, function (err, results) {
+    if (err) {
+      next(Error('Error rasterizing tables: ' + err.message))
+    }
+    console.log('\nTable created')
+    next()
+  })
+}
+
+function queryForCC (CC, callback) {
+  let WORDS = JSON.parse(fs.readFileSync(path.join(directories.src.countries, CC + '.json'), 'utf8'))
+
+  let queryCC = 'INSERT INTO ' + DB_INFO.db_tables.country_specs + ' ( ' +
+    'Country, ' +
+    'currency, ' +
+    'distance_std, ' +
+    'fuel_efficiency_std, ' +
+    'fuel_price_volume_std ' +
+    ') ' +
+    'VALUES ( ' +
+    '"' + CC + '", ' +
+    '"' + WORDS.curr_code + '", ' +
+    WORDS.distance_std_option + ', ' +
+    WORDS.fuel_efficiency_std_option + ', ' +
+    WORDS.fuel_price_volume_std +
+    ')'
+
+  db.query(queryCC, function (err, results, fields) {
+    if (err) {
+      callback(Error(err))
+    }
+    process.stdout.write(CC + ' ')
+    callback()
+  })
+}
+
+// main function of async.series([dbConnects, deletesTable, populatesTable, dbEnd]
+function dbEnd (next) {
+  db.end(function (err) {
+    if (err) {
+      next(Error('Error ending connection' + err.message))
+    }
+    next()
+  })
+}
