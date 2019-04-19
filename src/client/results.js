5e8db3c5f8cc2a40eb25a87da66529aaf2c8358f
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2019-04-19 19:15:43 +0200

    better organization of modules; runResults does not deal with HTML results, see: https://github.com/jfoclpf/autocosts/blob/master/contributing.md

--- src/client/results.js
+++ src/client/results.js
@@ -34,8 +34,103 @@ autocosts.resultsModule = (function (thisModule, translatedStrings, switches, la
     pdfModule = switches.pdf ? autocosts.resultsModule.pdfModule : {}
   }
 
-  function setCalculatedData (data) {
-    calculatedData = data
+  // Main show results function
+  function showResults (calculatedDataObj, form) {
+    var flattenedData, chartsDrawnPromisesObj, promisesArray
+
+    calculatedData = calculatedDataObj
+
+    // from complex object with hierarchies, flattens to simple object
+    // see for more info: https://github.com/hughsk/flat
+    flattenedData = commonsModule.flatten(calculatedData, { delimiter: '_' })
+    // it needs to show also 1/2 of Maintenance Costs
+    flattenedData.costs_perMonth_items_halfOfMaintenance = flattenedData.costs_perMonth_items_maintenance / 2
+    // console.log(flattenedData);
+    setCalculatedDataToHTML(flattenedData)
+
+    chartsDrawnPromisesObj = chartsModule.initialize(calculatedData)
+
+    $('#form').hide()
+
+    // The third of the three boxes on the top
+    // if financial effort was not calculated or not likely, does not show doughnut chart
+    // on the third box, and adapt the three boxes css classes
+    if (calculatedData.financialEffort.income.calculated && calculatedData.financialEffort.isLikelyToBeValid && switches.charts) {
+      chartsModule.drawDoughnutFinancialEffort(calculatedData)
+      // shows third box where the financial effort doughnut chart appears
+      $('#results #info-boxes .info-box.box-3').show()
+      $('#results #info-boxes .info-box').removeClass('two-boxes').addClass('three-boxes')
+    } else {
+      // hides third box where the financial effort doughnut chart appears
+      $('#results #info-boxes .info-box.box-3').hide()
+      $('#results #info-boxes .info-box').removeClass('three-boxes').addClass('two-boxes')
+    }
+
+    setPeriodicCosts(calculatedData, 'month')
+    setPeriodicCostsDetails(form, calculatedData) // the details on the dropdown boxes
+
+    // switches are frozen/const object in main.js, so no need to show elements when switches.charts is true
+    // since these elements are set tp be shown in css by default, just need to hide in case is false
+    if (switches.charts) {
+      chartsModule.drawCostsBars('month')
+      chartsModule.drawCostsDoughnut('month')
+    } else {
+      $('#results .costs-doughnut-chart, #results .costs-bars-chart-stats, #results .stats-references').hide()
+    }
+
+    // Financial Effort
+    if (calculatedData.financialEffort.calculated && calculatedData.financialEffort.isLikelyToBeValid) {
+      setFinancialEffortDetails(form, calculatedData)
+
+      // shows financial effort section
+      $('#results #financial-effort').show()
+
+      if (switches.charts) {
+        chartsModule.drawFinancialEffort(calculatedData)
+      } else {
+        $('#financial-effort .graph').hide()
+        $('#financial-effort .values.box').css('width', '40%').css('float', 'none')
+      }
+    } else {
+      // hides financial effort section
+      $('#results #financial-effort').hide()
+    }
+
+    // Equivalent transport costs
+    if (calculatedData.publicTransports.calculated) {
+      setEquivTransportCostsDetails(form, calculatedData)
+
+      $('#results #equivalent-transport-costs').show()
+
+      if (switches.charts) {
+        chartsModule.drawAlternativesToCar()
+      } else {
+        $('#equivalent-transport-costs .graph').hide()
+        $('#equivalent-transport-costs .values.box').css('margin', 'auto 2%').css('float', 'none')
+      }
+    } else {
+      $('#results #equivalent-transport-costs').hide()
+    }
+
+    setClassAccordionHandler()
+
+    $('#results').show()
+
+    $('*').promise().done(function () {
+      // it needs these promises, since the pdfMake body can only be generated when the charts are already fully drawn
+      // such that, the pdf generation can extract the charts to base64 images
+      promisesArray = Object.keys(chartsDrawnPromisesObj).map(function (key) {
+        return chartsDrawnPromisesObj[key]
+      })
+      promisesArray.push($('*').promise())
+      $.when.apply($, promisesArray).done(function () {
+        if (pdfModule && !$.isEmptyObject(pdfModule)) {
+          pdfModule.generatePDF(calculatedData)
+        }
+      })
+    })
+
+    return true
   }
 
   function getCostsColors () {
@@ -1014,14 +1109,7 @@ autocosts.resultsModule = (function (thisModule, translatedStrings, switches, la
   // thisModule, since this is a parent module and it may have been defined erlier by a children module
   thisModule.initialize = initialize
   thisModule.getCostsColors = getCostsColors
-
-  thisModule.setPeriodicCosts = setPeriodicCosts
-  thisModule.setCalculatedDataToHTML = setCalculatedDataToHTML
-  thisModule.setPeriodicCostsDetails = setPeriodicCostsDetails
-  thisModule.setFinancialEffortDetails = setFinancialEffortDetails
-  thisModule.setEquivTransportCostsDetails = setEquivTransportCostsDetails
-  thisModule.setClassAccordionHandler = setClassAccordionHandler
-  thisModule.setCalculatedData = setCalculatedData
+  thisModule.showResults = showResults
 
   return thisModule
 })(autocosts.resultsModule || {},
