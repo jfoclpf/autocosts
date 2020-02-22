748b9e54974811ce5a0944c84ccc11413bf06027
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2020-02-22 01:25:27 +0100

    create extra transfer function to use by JSDON

--- src/client/convertData.js
+++ src/client/convertData.js
@@ -419,6 +419,116 @@ autocosts.convertDataModule = (function (thisModule) {
     return userData
   }
 
+  // to be used by test script test/validateClient.js, particularly by JSDOM object
+  // to populate the web form (in jsdom) with user input data got from DB or a file
+  function createFormObjectFromDatabase (form, dbObject, countryInfo) {
+    var userData = createUserDataObjectFromDatabase(dbObject, countryInfo)
+    var f = form
+
+    // Form Part 1
+    // depreciation
+    f.acquisitionMonth.value = userData.depreciation.dateOfAcquisition.month
+    f.acquisitionYear.value = userData.depreciation.dateOfAcquisition.year
+    f.commercialValueAtAcquisition.value = userData.depreciation.dateOfAcquisition.valueOfTheVehicle
+    f.commercialValueAtNow.value = userData.depreciation.dateOfUserInput.valueOfTheVehicle
+
+    // insurance
+    f.insuranceValue.value = userData.insurance.period
+    f.insurancePaymentPeriod.value = userData.insurance.amountPerPeriod
+
+    // credit
+    f.AutoCreditRadioBtn.value = userData.credit.creditBool
+    f.borrowedAmount.value = userData.credit.yesCredit.borrowedAmount
+    f.numberInstallments.value = userData.credit.yesCredit.numberInstallments
+    f.amountInstallment.value = userData.credit.yesCredit.amountInstallment
+    f.residualValue.value = userData.credit.yesCredit.residualValue
+
+    // inspection
+    f.averageInspectionCost.value = userData.inspection.numberOfInspections
+    f.numberInspections.value = userData.inspection.averageInspectionCost
+
+    // roadTaxes
+    f.roadTaxes.value = userData.roadTaxes.amountPerYear
+
+    // Form Part 2
+    // fuel
+    f.radio_fuel_euros.value = userData.fuel.typeOfCalculation
+    f.fuel_currency_value.value = userData.fuel.currencyBased.amountPerPeriod
+    f.fuel_currency_time_period.value = userData.fuel.currencyBased.period
+    f.car_job_form2.value = userData.fuel.distanceBased.considerCarToJob
+    f.car_to_work_number_days_week.value = userData.fuel.distanceBased.carToJob.daysPerWeek
+    f.car_to_work_distance_home_work.value = userData.fuel.distanceBased.carToJob.distanceBetweenHomeAndJob
+    f.car_to_work_distance_weekend.value = userData.fuel.distanceBased.carToJob.distanceDuringWeekends
+    f.no_car_to_work_distance.value = userData.fuel.distanceBased.noCarToJob.distancePerPeriod
+    f.no_car_to_work_time_period.value = userData.fuel.distanceBased.noCarToJob.period
+    f.distance_standard_onfuel.value = userData.fuel.distanceBased.noCarToJob.distanceStandardUnit
+    f.fuel_efficiency.value = userData.fuel.distanceBased.fuelEfficiency
+    f.fuel_efficiency_standard_onfuel.value = userData.fuel.distanceBased.fuelEfficiencyStandard
+    f.fuel_price.value = userData.fuel.distanceBased.fuelPrice
+
+    // maintenance
+    f.maintenance.value = userData.maintenance.amountPerYear
+
+    // repairsImprovements
+    f.repairsImprovements.value = userData.repairsImprovements.amountPerYear
+
+    // parking
+    f.parking.value = userData.parking.amountPerMonth
+
+    // tolls
+    f.tolls_daily_radioBtn.value = userData.tolls.calculationBasedOnDay
+    f.daily_expense_tolls.value = userData.tolls.noBasedOnDay.amountPerPeriod
+    f.number_days_tolls.value = userData.tolls.noBasedOnDay.period
+    f.no_daily_tolls_value.value = userData.tolls.yesBasedOnDay.amountPerDay
+    f.tolls_period_select.value = userData.tolls.yesBasedOnDay.daysPerMonth
+
+    // fines
+    f.tickets_value.value = userData.fines.amountPerPeriod
+    f.tickets_period_select.value = userData.fines.period
+
+    // washing
+    f.washing_value.value = userData.washing.amountPerPeriod
+    f.washing_period_select.value = userData.washing.period
+
+    // Form Part 3
+    // publicTransports
+    f.household_number_people.value = userData.publicTransports.numberOfPeopleInFamily
+    f.public_transportation_month_expense.value = userData.publicTransports.monthlyPassCost
+
+    // income
+    f.radio_income.value = userData.income.incomePeriod
+    f.income_per_year.value = userData.income.year.amount
+    f.income_per_month.value = userData.income.month.amountPerMonth
+    f.income_months_per_year.value = userData.income.month.monthsPerYear
+    f.income_per_week.value = userData.income.week.amountPerWeek
+    f.income_weeks_per_year.value = userData.income.week.weeksPerYear
+    f.income_per_hour.value = userData.income.hour.amountPerHour
+    f.income_hours_per_week.value = userData.income.hour.hoursPerWeek
+    f.income_hour_weeks_per_year.value = userData.income.hour.weeksPerYear
+
+    // workingTime
+    f.radio_work_time.value = userData.workingTime.isActivated
+    f.time_month_per_year.value = userData.workingTime.monthsPerYear
+    f.time_hours_per_week.value = userData.workingTime.hoursPerWeek
+
+    // distance
+    f.drive_to_work.value = userData.distance.considerCarToJob
+    f.drive_to_work_days_per_week.value = userData.distance.carToJob.daysPerWeek
+    f.dist_home_job.value = userData.distance.carToJob.distanceBetweenHomeAndJob
+    f.journey_weekend.value = userData.distance.carToJob.distanceDuringWeekends
+    f.km_per_month.value = userData.distance.noCarToJob.distancePerPeriod
+    f.period_km.value = userData.distance.noCarToJob.period
+    f.distance_standard_ondistance.value = userData.distance.noCarToJob.distanceStandardUnit
+
+    // timeSpentInDriving:
+    f.time_home_job.value = userData.timeSpentInDriving.carToJob.minutesBetweenHomeAndJob
+    f.time_weekend.value = userData.timeSpentInDriving.carToJob.minutesDuringWeekend
+    f.min_drive_per_day.value = userData.timeSpentInDriving.noCarToJob.minutesPerDay
+    f.days_drive_per_month.value = userData.timeSpentInDriving.noCarToJob.daysPerMonth
+
+    return f
+  }
+
   function createDatabaseObjectFromForm (DOMform) {
     var databaseObj = {}
 
@@ -654,6 +764,7 @@ autocosts.convertDataModule = (function (thisModule) {
   thisModule.initialize = initialize
   thisModule.createUserDataObjectFromForm = createUserDataObjectFromForm
   thisModule.createUserDataObjectFromDatabase = createUserDataObjectFromDatabase
+  thisModule.createFormObjectFromDatabase = createFormObjectFromDatabase
   thisModule.createDatabaseObjectFromForm = createDatabaseObjectFromForm
 
   return thisModule
