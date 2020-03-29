a5edc2d35237a283a51f334633e8c5d9fb92e7ce
joao <joao.pimentel.ferreira@gmail.com>
2020-03-29 14:55:58 +0200

    improve test of API

--- test/testApi.js
+++ test/testApi.js
@@ -2,10 +2,10 @@ const path = require('path')
 
 console.log('\nValidating main API')
 
-const autocosts = require(path.join('..', 'app'))
+const autocosts = require(path.join('..', 'api'))
 
 // see https://github.com/jfoclpf/autocosts/blob/master/contributing.md#userdata-class
-var userDataTest = {
+var userDataTest1 = {
   countryCode: 'US',
   currency: 'USD',
 
@@ -170,9 +170,113 @@ var userDataTest = {
   }
 }
 
+var userDataTest2 = {
+  countryCode: 'US',
+  currency: 'USD',
+
+  depreciation: {
+    dateOfAcquisition: {
+      month: 5,
+      year: 2001,
+      valueOfTheVehicle: 25000
+    },
+    dateOfUserInput: {
+      month: 2,
+      year: 2020,
+      valueOfTheVehicle: 5000
+    }
+  },
+
+  insurance: {
+    amountPerPeriod: 200,
+    period: 'month'
+  },
+
+  credit: {
+    creditBool: true,
+    yesCredit: {
+      borrowedAmount: 15000,
+      numberInstallments: 48,
+      amountInstallment: 350,
+      residualValue: 0
+    }
+  },
+
+  inspection: {
+    averageInspectionCost: 120,
+    numberOfInspections: 15
+  },
+
+  roadTaxes: {
+    amountPerYear: 120
+  },
+
+  // Form Part 2
+  fuel: {
+    typeOfCalculation: 'distance', // type string: "money" or "distance"
+    currencyBased: {
+      amountPerPeriod: null,
+      period: null // type string: "month", "twoMonths",  "trimester", "semester", "year"
+    },
+    distanceBased: {
+      considerCarToJob: true, // boolean
+      carToJob: {
+        daysPerWeek: 5,
+        distanceBetweenHomeAndJob: 15,
+        distanceDuringWeekends: 30,
+        distanceStandardUnit: 'mi' // standard distance for current country: "km", "mil" or "mil(10km)"
+      },
+      noCarToJob: {
+        distancePerPeriod: null,
+        period: null, // type string: "month", "twoMonths",  "trimester", "semester", "year"
+        distanceStandardUnit: null // type string: "km", "mil" or "mil(10km)"
+      },
+      fuelEfficiency: 25, // fuel efficiency of the vehicle
+      fuelEfficiencyStandard: 'mpg(US)', // type string; "ltr/100km", "mpg(US)", etc.
+      fuelPrice: 2.5, // type number; currency per unit of volume standard. Ex: 1.4, that is 1.4 EUR / ltr
+      fuelPriceVolumeStandard: 'gal(US)' // type string: 'ltr', 'gal(UK)', 'gal(US)'
+    }
+  },
+
+  maintenance: {
+    amountPerYear: 700
+  },
+
+  repairsImprovements: {
+    amountPerYear: 200
+  },
+
+  parking: {
+    amountPerMonth: 14
+  },
+
+  tolls: {
+    calculationBasedOnDay: true, // true or false
+    yesBasedOnDay: {
+      amountPerDay: 2.5,
+      daysPerMonth: 22
+    },
+    noBasedOnDay: {
+      amountPerPeriod: null,
+      period: null // type string: "month", "twoMonths",  "trimester", "semester", "year"
+    }
+  },
+
+  fines: {
+    amountPerPeriod: 40,
+    period: 'year' // type string: "month", "twoMonths",  "trimester", "semester", "year"
+  },
+
+  washing: {
+    amountPerPeriod: 110,
+    period: 'year' // type string: "month", "twoMonths",  "trimester", "semester", "year"
+  }
+}
+
 try {
-  var results = autocosts.calculate(userDataTest)
-  if (results.costs.perMonth.total) {
+  var results1 = autocosts.calculate(userDataTest1)
+  var results2 = autocosts.calculate(userDataTest2)
+  if (results1.costs.perMonth.total && results2.costs.perMonth.total) {
     console.log('Test on API run successfully', '\n')
     process.exit(0)
   } else {
